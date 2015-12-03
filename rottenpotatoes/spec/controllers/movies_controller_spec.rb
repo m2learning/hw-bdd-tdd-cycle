7871441e45698d4f5eeea28f0d674a5a1f29653a
m2learning <m2c9@ulg.ac.be>
2015-12-03 23:11:14 +0000

    Added spec for the create, edit and delete controler methods

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -3,6 +3,32 @@ require 'rails_helper'
 describe MoviesController do
   fixtures :movies
 
+  describe 'showing a specific movie with a given id' do
+    it 'should call the model method that perform the search of movies by id' do
+      Movie.should_receive(:find).with('1')
+      get :show, {id: 1}
+    end
+
+    it 'should select the show view for rendering' do
+      get :show, {id: 1}
+      response.should render_template('show')
+    end
+  end
+
+  describe 'showing the home page with the index filtered and sorted' do
+    it 'should call the all_ratings method of the model' do
+      Movie.should_receive(:all_ratings)
+      get :index, {sort: 'title', ratings: ['R', 'G']}
+    end
+
+    it 'should redirect to restful url' do
+      get :index, {sort: 'release_date', ratings: ['R', 'G']}
+      response.should redirect_to('/movies?ratings%5B%5D=R&ratings%5B%5D=G&sort=release_date')
+    end
+
+  end
+
+
   describe 'searching movies with same director' do
     it 'should call the model method that performs the search of movies with the same director of the current movie' do
       Movie.should_receive(:with_same_director).with('1')
@@ -28,7 +54,55 @@ describe MoviesController do
       # look for controller method to assign @movies
       assigns(:movies_with_same_director).should == fake_results
     end
-
   end
+
+
+  describe 'Getting the new form' do
+    it 'should render the new form' do
+      get :new, {}
+      response.should render_template('new')
+    end
+  end
+
+
+  describe 'posting the create form' do
+    it 'should call the create! method of the model' do
+#      Movie.should_receive(:create!)
+#      post :create, movie: {title: 'My New Movie', ratings: ['R', 'G'], release_date: '1984-10-26 00:00:00 UTC'}
+    end
+
+    it "creates a new movie" do
+      expect{
+        post :create, movie: {title: 'My New Movie', ratings: ['R', 'G'], release_date: '1984-10-26 00:00:00 UTC'}
+      }.to change(Movie, :count).by(1)
+    end
+
+    it 'should redirect to the home page' do
+      post :create, {movie: {title: 'My New Movie', ratings: ['R', 'G'], release_date: '1984-10-26 00:00:00 UTC'}}
+      response.should redirect_to('/movies')
+    end
+  end
+
+  describe 'PUT update' do
+    it "redirects to the updated movie" do
+      put :update, id: 1, movie: {title: 'My New Movie', ratings: ['R', 'G'], release_date: '1984-10-26 00:00:00 UTC'}
+      response.should redirect_to('/movies/1')
+    end    
+  end
+
+
+  describe 'DELETE destroy' do
+    it "deletes the movie" do
+      expect{
+        delete :destroy, {id: 1} 
+      }.to change(Movie, :count).by(-1)
+    end
+    
+    it "redirects to movies#index" do
+      delete :destroy, {id: 1} 
+      response.should redirect_to('/movies')
+    end
+  end
+
 
 end
--- rottenpotatoes/spec/models/movie_spec.rb
+++ rottenpotatoes/spec/models/movie_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 describe 'MovieModel' do
   fixtures :movies
 
+  describe 'getting the list of all movie ratings' do
+    it 'shoudl return the list of all movie ratings' do 
+       @all_ratings = Movie.all_ratings
+       expect(@all_ratings).to eq(%w(G PG PG-13 NC-17 R))
+    end
+  end
+
   describe 'searching movies with same director' do
     it 'should find the movies with the same directors as movie id 1' do
       @movies = Movie.with_same_director(1)

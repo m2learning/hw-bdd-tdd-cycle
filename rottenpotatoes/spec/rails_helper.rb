f9441535d2ec7c50769374c9488531b7cf872ae8
m2learning <m2c9@ulg.ac.be>
2015-12-07 21:02:03 +0000

    modif rails_helper

--- rottenpotatoes/spec/rails_helper.rb
+++ rottenpotatoes/spec/rails_helper.rb
@@ -52,4 +52,11 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+
+  # Added by m2learning
+  config.infer_base_class_for_anonymous_controllers = false
+
 end
+
+
